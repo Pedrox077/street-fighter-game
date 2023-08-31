8065a81d297af8bea28d60f79a2067ed08af8c8f
unknown <pedrovieira7070@gmail.com>
2023-08-31 14:23:30 -0300

    teste commit

---
+++ src/entities/FpsCounter.js
@@ -0,0 +1,16 @@
+export class FpsCounter {
+    constructor() {
+        this.fps = 0;
+    }
+
+update(time) {
+    this.fps = Math.trunc(1 / time.secondsPassed);
+}
+
+draw(context) {
+    context.front = '14px Arial';
+    context.fillStyle = '#0FF00';
+    context.textAlign = 'right';
+    context.fillText(`${this.fps}`, context.canvas.width - 2, context.canvas.height - 2);
+    }
+}
